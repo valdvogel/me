0c478bddf936757d8ad4b40e8f580bcfbf952efe
Jose Valdvogel <valdvogel@hotmail.com>
2020-05-25 22:30:34 -0700

    Updates

---
+++ precache-manifest.9f9a8bc2f97df7acc33c2aa78ab5f0e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "cf0dd2c32d090971b600",
+    "url": "/me/static/js/main.cf0dd2c3.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "cf0dd2c32d090971b600",
+    "url": "/me/static/css/main.5d7ad904.chunk.css"
+  },
+  {
+    "revision": "38525606c52e5c14cea7a1e7b50ffe7a",
+    "url": "/me/index.html"
+  }
+];
